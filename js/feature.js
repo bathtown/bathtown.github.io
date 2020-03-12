431fbd92dc94dc14d5e047d344f0ad33f9deeca8
hyf <hyf-0418@outlook.com>
2020-03-12 16:27:15 +0800

    modify feature.js

--- js/feature.js
+++ js/feature.js
@@ -1,5 +1,6 @@
 // for safari
-document.body.addEventListener('touchstart', function () {}, false);
+document.body.addEventListener('touchstart', function () {
+}, false);
 
 // function replacePic() {
 //     var imgs = document.images; // All images
@@ -35,8 +36,8 @@ function likedPlus() {
     const likedNumberObj = document.getElementById("likedNumber");
     let num = likedNumberObj.textContent;
     const heart = document.getElementById("liking");
-    if (heart.style.color == "rgb(255, 225, 225)" ||
-        heart.style.color == "") {
+    if (heart.style.color === "rgb(255, 225, 225)" ||
+        heart.style.color === "") {
         num++;
         heart.style.color = "orangered";
     } else {
@@ -51,22 +52,22 @@ function likedPlus() {
 function swap() {
     document.getElementsByClassName("de_choice de_myProperties")[0].style.display = "none";
 
-    var choice1 = document.getElementById("description");// 上面两个tab
-    var choice2 = document.getElementById("properties");
+    const choice1 = document.getElementById("description");// 上面两个tab
+    const choice2 = document.getElementById("properties");
 
     choice1.onclick = function () {
         document.getElementsByClassName("de_choice de_myProperties")[0].style.display = "none";
         document.getElementsByClassName("de_choice de_myWords")[0].style.display = "block";
         choice1.className = "selected";
         choice2.className = "";
-    }
+    };
 
     choice2.onclick = function () {
         document.getElementsByClassName("de_choice de_myProperties")[0].style.display = "inline-table";
         document.getElementsByClassName("de_choice de_myWords")[0].style.display = "none";
         choice1.className = "";
         choice2.className = "selected";
-    }
+    };
 }
 
 swap();
